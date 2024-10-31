b93fc5b49c34ba5a44928aa084f1d535cfde0521
Tomas Helmfridsson <tomas.helmfridsson@addq.se>
2024-10-31 10:56:39 +0100

    Update ArrayExample.java

--- ArrayExample.java
+++ ArrayExample.java
@@ -1,10 +1,12 @@
 public class ArrayExample {
     public static void main(String[] args) {
+        String text;
         int[] numbers = {1, 2, 3, 4, 5};
 
         for (int i = 0; i <= numbers.length; i++) {
             System.out.println(numbers[i]);   
 
         }
+        System.out.println(text); 
     }
 }
