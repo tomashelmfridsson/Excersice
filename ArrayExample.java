d8e52e4afc5353f3eb185644315116fcafd98d74
Tomas Helmfridsson <tomas.helmfridsson@addq.se>
2024-10-31 10:55:33 +0100

    Create ArrayExample.java

---
+++ ArrayExample.java
@@ -0,0 +1,10 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i <= numbers.length; i++) {
+            System.out.println(numbers[i]);   
+
+        }
+    }
+}
